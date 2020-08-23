8e8ead624d8c020e884be22201c6e22663de1ca4
Inti Dev <yeseniase@outlook.it>
2020-08-23 13:51:03 -0500

    [ADD] react-boostrap with components test

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React from 'react';
+import { Container, Row, Col, Image } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import logo from './logo.png';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-    </div>
+    <Container fluid>
+      <Row className="App-header">
+        <Col  md={{ span: 2, offset: 2 }}>
+          Top gifs
+        </Col>
+      </Row>
+      <Row>
+        <Col md={{ span: 2, offset: 5 }}>
+          <Image className="App-logo p-3" src={logo} fluid alt="logo" />
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
